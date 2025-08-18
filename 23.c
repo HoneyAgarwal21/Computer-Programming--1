50d3db1c36944f76bb17fa0436b215d3fbc086ef
HoneyAgarwal21 <honey2121agarwal@gmail.com>
2025-08-18 23:41:19 +0530

    23.c

--- 23.c
+++ 23.c
@@ -4,12 +4,13 @@ void main()
     int a,b,c,d,e,f;
     printf("Marks Of First Subject :");
     scanf("%d",&a);
-    printf("Marks Of First Subject :");
+    printf("Marks Of Second Subject :");
     scanf("%d",&b);
-    printf("Marks Of First Subject :");
+    printf("Marks Of Third Subject :");
     scanf("%d",&c);
     d=a+b+c;
     e=d/3;
     printf("Average Marks :%d\n",e);
     printf("Total Marks :%d",d);
+
 }
